62572b4166c3691ff142beabc70d803ba7093e10
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-24 14:20:09 +0530

    Add files via upload

---
+++ CONSTA~1.C
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+#include<conio.h>
+
+#define PI 3.14
+void main()
+{
+	int r=3;
+	float ratio;
+   // const float PI=3.14;
+   clrscr();
+     ratio=2*PI*r;
+    printf("The value of PI is: %f \n",PI);
+    printf("ratio =%f",ratio);
+    getch();  
+}     
