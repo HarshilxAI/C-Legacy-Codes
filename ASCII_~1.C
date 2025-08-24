0c1b142477d151c65e66da60dd4c596e4b868f5f
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-24 14:19:39 +0530

    Add files via upload

---
+++ ASCII_~1.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+  
+void main()  
+{  
+
+      char ch;
+      clrscr();
+
+     printf("Enter a character");  
+    scanf("%c",&ch);   
+    
+     printf("\n The ascii value of the ch variable is : %d", ch);  
+    getch();  
+}  
