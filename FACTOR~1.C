7ff82ec6c71ea424cb3f9813276f931b6e9170da
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-25 19:31:28 +0530

    Add files via upload

---
+++ FACTOR~1.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int n, i, fact = 1;
+    clrscr();
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+
+    if (n < 0)
+    {
+	printf("Error! Factorial of a negative number doesn't exist.");
+    }
+    else
+    {
+	for (i = 1; i <= n; i++)
+	{
+	    fact=fact*i;
+	}
+	printf("Factorial of %d is = %d ",n,fact);
+    }
+
+	getch();
+}
