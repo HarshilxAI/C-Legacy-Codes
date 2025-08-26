dc229ab98de2a97ac1b30eb5fa08e7879ef2c957
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-26 19:15:57 +0530

    Add files via upload

---
+++ IF1.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    
+	int n;
+	clrscr();
+
+	printf("Enter a number:");    
+	scanf("%d",&n);
+
+	if(n<=10)
+	{
+		printf("given no is less than or equal 10");
+	}
+
+	getch();
+}
