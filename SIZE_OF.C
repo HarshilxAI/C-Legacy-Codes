108bb613e779b344de665f9af17012bfde7e0a1b
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-29 19:22:15 +0530

    Add files via upload

---
+++ SIZE_OF.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x=89;
+	clrscr();
+	printf("size  of variable %d",sizeof(x));
+	printf("\nsize  of integer %d",sizeof(int));
+	printf("\nsize  of character %d",sizeof(char));
+	printf("\nsize  of float %d",sizeof(float));
+
+	getch();
+}
