051aff5e5a635e7a094d26c8e1fd24e64c2d37fe
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-31 14:31:29 +0530

    Add files via upload

---
+++ STRUCT1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+	int rno;
+	char name[50];
+}s1;
+
+void main()
+{
+
+	//struct student s1;
+	clrscr();
+	s1.rno=101;
+	strcpy(s1.name,"darshil");
+
+	printf("\nstudent rollno=%d",s1.rno);
+	printf("\nstudent name=%s",s1.name);
+
+	getch();
+}
