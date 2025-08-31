05f96cd372551723aed19315e905625826175b82
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-31 14:32:36 +0530

    Add files via upload

---
+++ STUD_INF.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char n,add,bdate;
+	clrscr();
+	printf("enter name=\n");
+	scanf("%s",&n);
+	printf("\n enter address=\n");
+	scanf("%s",&add);
+	printf("\n enter birth date=\n");
+	scanf("%s",&bdate);
+
+	printf("name=%c",n);
+	ptintf("\n address=%c",add );
+	printf("\n birth date=%c",bdate);
+
+	getch();
+}
