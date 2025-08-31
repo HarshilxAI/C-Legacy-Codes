57007180af6fcbad323c0c39355b0958450cc294
HARSHIL GURJAR <hdgurjar2323@gmail.com>
2025-08-31 14:32:08 +0530

    Add files via upload

---
+++ STRUCTUR.C
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+struct	 address
+{
+	int pin;
+	char city[20];
+	char phone[14];
+};
+struct employee
+{
+	char name[20];
+	struct address add;
+};
+void display(struct employee);
+ void main()
+ {
+	struct employee e1;
+	clrscr();
+	printf("\n enter employee info\n");
+	scanf("%s",e1.name);
+	printf("\nenter city=");
+	strcpy(e1.add.city,"jiyansh");
+	printf("\nenter pin=");
+	scanf("%d",&e1.add.pin);
+	printf("\nenter phone=");
+	scanf("%s",e1.add.phone);
+
+	display(e1);
+	getch();
+ }
+
+ void display(struct employeee e1)
+ {
+	printf("\n detail is......");
+	printf("\n name=%s",e1.name);
+	printf("\n  city=%s",e1.add.city);
+	printf("\n pin=%d",e1.add.pin);
+	printf("\n phone=%s",e1.add.phone);
+ }
